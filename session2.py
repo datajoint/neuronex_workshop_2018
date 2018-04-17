eeb58fb6c74de33c71ba19b3c48b3e10199069ea
Edgar Y. Walker <edgar.walker@gmail.com>
2018-04-17 02:14:35 +0000

    Update contents

--- session2.py
+++ session2.py
@@ -1,7 +1,7 @@
 import datajoint as dj
 
 username = dj.config['database.user']
-schema = dj.schema('{}_workshop'.format(username))
+schema = dj.schema('{}_pipeline_session2'.format(username))
 
 
 
@@ -11,7 +11,7 @@ schema = dj.schema('{}_workshop'.format(username))
 class Mouse(dj.Manual):
     definition = """
     # Experimental animals
-    mouse_id2             : int                          # Unique animal ID
+    mouse_id             : int                          # Unique animal ID
     ---
     dob=null             : date                         # date of birth
     sex="unknown"        : enum('M','F','unknown')      # sex
@@ -28,16 +28,6 @@ class Session(dj.Manual):
     experiment_setup           : int                          # experiment setup ID
     experimenter               : varchar(100)                 # experimenter name
     """
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 
 # Insert the following data into the table
@@ -78,34 +68,5 @@ session_data = [
   'mouse_id': 100,
   'session_date': "2017-05-25"}]
 
-try:
-    Mouse.insert(mouse_data, skip_duplicates=True)
-    Session.insert(session_data, skip_duplicates=True)
-except:
-    print('Non matchin table definition. Creating fixed version')
-    # Issue encountered. Defining all tables in different schema
-    schema = dj.schema('{}_workshop_fix'.format(username))
-    @schema
-    class Mouse(dj.Manual):
-        definition = """
-        # Experimental animals
-        mouse_id             : int                          # Unique animal ID
-        ---
-        dob=null             : date                         # date of birth
-        sex="unknown"        : enum('M','F','unknown')      # sex
-        mouse_notes=""       : varchar(4096)                # other comments and distinguishing features
-        """
-
-    @schema
-    class Session(dj.Manual):
-        definition = """
-        # Experiment session
-        -> Mouse
-        session_date               : date                         # date
-        ---
-        experiment_setup           : int                          # experiment setup ID
-        experimenter               : varchar(100)                 # experimenter name
-        """
-    
-    Mouse.insert(mouse_data, skip_duplicates=True)
-    Session.insert(session_data, skip_duplicates=True)
+Mouse.insert(mouse_data, skip_duplicates=True)
+Session.insert(session_data, skip_duplicates=True)
