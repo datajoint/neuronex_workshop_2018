d9c67acc901b3f063c9ddc24ba463d69fb08a301
Carlos Brody <carlosbrody@gmail.com>
2020-01-13 16:09:57 -0500

    Finalized and cleaned up

--- julia/setup.jl
+++ julia/setup.jl
@@ -1,4 +1,15 @@
-cd("/Users/carlos/Github/datajoint/neuronex_workshop_2018/julia")
+###########################
+#
+# This code starts from scratch to connect to the database and set up and populate some tables,
+# to be used in the Julia tutorial Jupyter notebook "02-Imported and Computed Tables.ipynb"
+#
+# We assume that you have an appropriately configures dj_local_conf.json file in your working directory
+#
+###########################
+
+
+cd("/Users/carlos/Github/datajoint/neuronex_workshop_2018/julia")  # Insert your own working directory here!
+# Your directory should have the file "d2j.jl" in it.
 
 using PyCall
 dj = pyimport("datajoint")
@@ -19,17 +30,27 @@ schema = dj.schema(schema_name, py"locals()")
 
 
 py"""
-@$schema
-class Mouse($dj.Manual):
-      definition = '''
+schema = $dj.schema($schema_name, locals())
+"""
+
+@pydef mutable struct Mouse <: dj.Manual
+    definition = """
       mouse_id: int                  # unique mouse id
       ---
       dob: date                      # mouse date of birth
       sex: enum('M', 'F', 'U')       # sex of mouse - Male, Female, or Unknown/Unclassified
-      '''
-"""
+      """
+end
 
-mouse = py"Mouse()"
+# Julia doesn't have decorators, so we do the @schema decoration by hand in Python:
+py"""
+Mouse = schema($Mouse)
+"""
+# And then make sure the Julia variable is the new Python Mouse
+Mouse = py"Mouse"
+
+
+mouse = Mouse()
 
 
 mouse.insert([
@@ -40,27 +61,28 @@ mouse.insert([
         Dict("mouse_id"=>5,   "dob"=>"2020-01-05", "sex"=>"M"),
         Dict("mouse_id"=>6,   "dob"=>"2020-01-05", "sex"=>"F"),
         Dict("mouse_id"=>7,   "dob"=>"2020-01-05", "sex"=>"F"),
+        Dict("mouse_id"=>8,   "dob"=>"2018-01-05", "sex"=>"U"),
         Dict("mouse_id"=>100, "dob"=>"2017-01-05", "sex"=>"F")
         ], skip_duplicates=true)
 
 
-py"""
-schema = $dj.schema($schema_name, locals())
-
-@schema
-class Session($dj.Manual):
-    definition = '''
+@pydef mutable struct Session <: dj.Manual
+    definition = """
     # Experiment session
     -> Mouse
     session_date               : date                         # date
     ---
     experiment_setup           : int                          # experiment setup ID
     experimenter               : varchar(100)                 # experimenter name
-    '''
+    """
+end
 
+py"""
+Session = schema($Session)
 """
+Session = py"Session"
 
-session = py"Session"()
+session = Session()
 
 data = Dict(
   "mouse_id" => 0,
@@ -69,9 +91,8 @@ data = Dict(
   "experimenter" => "Edgar Y. Walker"
 )
 
-##
 session.insert1(data, skip_duplicates=true)
-## 
+
 
 data = [
     Dict(
@@ -83,12 +104,6 @@ data = [
     Dict(
   "mouse_id" => 100,
   "session_date" => "2017-05-25",
-  "experiment_setup" => 0,
-  "experimenter" => "Boaty McBoatFace"
-    ),
-    Dict(
-  "mouse_id" => 100,
-  "session_date" => "2017-06-01",
   "experiment_setup" => 0,
   "experimenter" => "Boaty McBoatFace"
     ),
